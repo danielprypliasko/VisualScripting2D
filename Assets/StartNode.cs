c582feca14bf9d3de3ef818ab7e0dcb86a1002ca
zjs901 <zakjs901@gmail.com>
2026-05-04 02:30:54 +0100

    Added 2 scenes

--- Assets/StartNode.cs
+++ Assets/StartNode.cs
@@ -1,114 +1,30 @@
-using System.Collections;
+using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class StartNode : Node
+public class StartNode : Node<object, int>
 {
-    [SerializeField] private bool fireOnStart;
-    [SerializeField] private bool loop;
-    [SerializeField] private float interval = 1f;
-    [SerializeField] private Transform outputPoint;
 
-    private Coroutine pulseRoutine;
-
-    [SerializeField] private Transform player;
-
-    protected override void OnEnable()
+    private int wattage = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created  
+    void Start()
     {
-        base.OnEnable();
-        RestartPulseLoop();
-    }
-    
-    private void Update()
-    {
-        if (!Keyboard.current.eKey.wasPressedThisFrame) return;
 
-        float distance = Vector2.Distance(transform.position, player.position);
-        if (distance <= 2)
-        {
-            Pulse(CreateContext());
-        }
     }
 
-    private void OnDisable()
+    // Update is called once per frame  
+    void Update()
     {
-        StopPulseLoop();
+
     }
 
-   
-
-    public override void Execute(string inputPort, NodeFlowContext context)
+    public override int FeedOutputs(int watts)
     {
+        return watts;
     }
 
-    public override NodeValue Evaluate(string outputPort, NodeFlowContext context)
+    public override object TakeInputs()
     {
-        return NodeValue.Flow;
-    }
-
-    public override Transform GetInputAnchor(string port)
-    {
-        return null;
-    }
-
-    public override Transform GetOutputAnchor(string port)
-    {
-        return NormalizePort(port, FlowOutPort) == FlowOutPort ? ResolveAnchor(outputPoint) : null;
-    }
-
-    [ContextMenu("Pulse")]
-    public void Pulse()
-    {
-        Pulse(CreateContext());
-    }
-
-    private void RestartPulseLoop()
-    {
-        StopPulseLoop();
-
-        if (!Application.isPlaying || !fireOnStart)
-        {
-            return;
-        }
-
-        pulseRoutine = StartCoroutine(PulseLoop());
-    }
-
-    private void StopPulseLoop()
-    {
-        if (pulseRoutine == null)
-        {
-            return;
-        }
-
-        StopCoroutine(pulseRoutine);
-        pulseRoutine = null;
-    }
-
-    private IEnumerator PulseLoop()
-    {
-        yield return null;
-
-        var delay = Mathf.Max(0.02f, interval);
-
-        while (enabled)
-        {
-            Pulse(CreateContext());
-
-            if (!loop)
-            {
-                break;
-            }
-
-            yield return new WaitForSeconds(delay);
-        }
-
-        pulseRoutine = null;
-    }
-
-    private void Pulse(NodeFlowContext context)
-    {
-        PulseVisual();
-        Emit(FlowOutPort, context);
+        return new object();
     }
 }
