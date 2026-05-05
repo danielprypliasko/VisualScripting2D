312038e78f1560c683e43118a58dba5f3d15651c
OliverLudford <oliverlukeludford@gmail.com>
2026-05-05 19:17:14 +0100

    Xp Update

--- Assets/OpenGateNode.cs
+++ Assets/OpenGateNode.cs
@@ -1,16 +1,26 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class GateNode : Node
 {
     [SerializeField] private Transform inputPoint;
     [SerializeField] private Animator gateAnimator;
+    [SerializeField] private float xpReward = 25f;
+    private bool hasGivenXP = false;
 
     public override void Execute(string inputPort, NodeFlowContext context)
     {
         if (!BeginExecution(ref context))
         {
             return;
+        }
+
+        if (XPController.instance != null && !hasGivenXP)
+        {
+            hasGivenXP = true;
+            XPController.instance.AddXp(xpReward);
+
         }
 
         bool canOpen = context.Graph.Get("value", NodeValue.FromBool(false)).AsBool();
